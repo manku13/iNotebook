875ea91801813be08e3cca56b1d9e74a50ad7cb8
Mayank Soni <sonimayank151@gmail.com>
2024-05-31 20:41:36 +0530

    Initial commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+        .then(() => {
+            console.log("Connected to Mongo Successfully");
+        })
+        .catch((error) => {
+            console.error("Failed to connect to Mongo", error);
+        });
+}
+
+module.exports = connectToMongo;
